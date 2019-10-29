0cce7f69e1cbfd024990779341bb83da0fbaaf5b
Richard Gibert <richard@gibert.ca>
2019-10-28 20:06:04 -0400

    cleaned up lint errors

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,22 +1,33 @@
+#!/usr/bin/env python
+"""Molecule test cases"""
+
+
 import os
 
 import testinfra.utils.ansible_runner
 
+# pylint: disable=invalid-name
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
 def test_bin_installed(host):
-    f = host.file('/usr/local/share/prometheus-pihole-exporter/pihole_exporter-linux-amd64')
-    assert f.is_file
-    assert f.user == 'pi'
-    assert f.group == 'pi'
-    assert oct(f.mode) == '0755'
+    """Tests that the exporter binary was installed properly"""
+    host_file = host.file(
+        '/usr/local/share/prometheus-pihole-exporter/pihole_exporter-linux-amd64'
+    )
+    assert host_file.is_file
+    assert host_file.user == 'pi'
+    assert host_file.group == 'pi'
+    assert oct(host_file.mode) == '0755'
 
 
 def test_service_installed(host):
-    f = host.file('/etc/systemd/system/prometheus-pihole-exporter.service')
-    assert f.is_file
-    assert f.user == 'root'
-    assert f.group == 'root'
-    assert oct(f.mode) == '0644'
+    """Tests that the systemd service file was installed properly"""
+    host_file = host.file(
+        '/etc/systemd/system/prometheus-pihole-exporter.service'
+    )
+    assert host_file.is_file
+    assert host_file.user == 'root'
+    assert host_file.group == 'root'
+    assert oct(host_file.mode) == '0644'
