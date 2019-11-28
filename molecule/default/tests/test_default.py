55b1659c777acc37cb5c1107685f5e97cf5d0539
Richard Gibert <richard@gibert.ca>
2019-11-27 22:09:04 -0500

    initial role setup

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,34 +1,27 @@
-#!/usr/bin/env python
-"""Molecule test cases"""
-
-
 import os
 
 import testinfra.utils.ansible_runner
 
-# pylint: disable=invalid-name
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_bin_installed(host):
-    """Tests that the exporter binary was installed properly"""
-    host_file = host.file(
-        '/usr/local/share/prometheus-pihole-exporter/' +
-        'pihole_exporter-linux-amd64'
-    )
-    assert host_file.is_file
-    assert host_file.user == 'pi'
-    assert host_file.group == 'pi'
-    assert oct(host_file.mode) == '0755'
+def test_bin_file(host):
+    assert host.file(
+        "/usr/local/share/prometheus_pihole_exporter-0.0.6"
+        "/pihole_exporter-linux-amd64"
+    ).exists
 
 
-def test_service_installed(host):
-    """Tests that the systemd service file was installed properly"""
-    host_file = host.file(
-        '/etc/systemd/system/prometheus-pihole-exporter.service'
-    )
-    assert host_file.is_file
-    assert host_file.user == 'root'
-    assert host_file.group == 'root'
-    assert oct(host_file.mode) == '0644'
+def test_service_file(host):
+    assert host.file(
+        "/etc/systemd/system/prometheus_pihole_exporter.service"
+    ).exists
+
+
+def test_service_enabled(host):
+    assert host.service('prometheus_pihole_exporter').is_enabled
+
+
+def test_service_running(host):
+    assert host.service('prometheus_pihole_exporter').is_running
